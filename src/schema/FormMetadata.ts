b1203885e4a416de29137a34fa0125ecb0c0011b
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-10-05 17:12:01 +1000

    json schema loaidng in progress

---
+++ src/schema/FormMetadata.ts
@@ -0,0 +1,14 @@
+export class RowMetadata {
+    public controls: any[];
+}
+
+export class ColumnMetadata {
+    public large: number;
+    public medium: number;
+    public small: number;
+    public controls: any[];
+}
+
+export class DropDownMetadata {
+    
+}
